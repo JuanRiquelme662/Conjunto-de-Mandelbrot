3de12687eff2b527af05a6b5bba38fcd34ddcaf6
Juan Riquelme Xavier de Souza <juanriquelmexs2007@gmail.com>
2026-08-26 20:31:37 -0300

    estruturacao da logica basica do loop princiapl

---
+++ main.c
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+
+int main(){
+
+for(int linha = 0; linha < altura; linha++){
+    for(int coluna = 0; coluna < largura; coluna++){
+
+        //calculo do c desse pixel
+        double cr = -2.0 + (coluna / (double)(largura - 1)) * 3.0;
+        double ci = -1.5 + (linha / (double)(altura - 1)) * 3.0;
+
+        //zera as variaves
+        double zr = 0, zi = 0;
+        int num_interacoes = 0;
+        //esse (zr*zr + zi*zi) <= 4 e uma forma simplificada de ver se o numero saiu do conjunto de mandlebrot
+        //sem ter que depender de raiz quadrada
+        while (num_interacoes < max_num_interacoes && (zr*zr + zi*zi) <= 4) {
+
+            double zr_novo = zr*zr - zi*zi + cr;
+            double zi_novo = 2*zr*zi + ci;
+            zr = zr_novo;
+            zi = zi_novo;
+            num_interacoes++;
+
+        }
+
+    }
+}
+
+    return 0;
+}
