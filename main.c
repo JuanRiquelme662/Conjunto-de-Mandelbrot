83f66966c9e2f8287511a3cc9757d8eba7046cea
Juan Riquelme Xavier de Souza <juanriquelmexs2007@gmail.com>
2026-08-27 22:57:44 -0300

    criacao de uma funcao para a construcao da imagem de mandelbrot

--- main.c
+++ main.c
@@ -3,34 +3,36 @@
 
 
 
+//a linha abaixo e o necessario para virar opemmp, mas vou ver isso melhor amanha
+//#pragma omp parallel for num_threads(num_threads)
+void mandlebrot_serial(){
+    for(int linha = 0; linha < altura; linha++){
+        for(int coluna = 0; coluna < largura; coluna++){
 
+            //calculo do c desse pixel
+            double cr = -2.0 + (coluna / (double)(largura - 1)) * 3.0;
+            double ci = -1.5 + (linha / (double)(altura - 1)) * 3.0;
 
-int main(){
+            //zera as variaves
+            double zr = 0, zi = 0;
+            int num_interacoes = 0;
+            //esse (zr*zr + zi*zi) <= 4 e uma forma simplificada de ver se o numero saiu do conjunto de mandlebrot
+            //sem ter que depender de raiz quadrada
+            while (num_interacoes < max_num_interacoes && (zr*zr + zi*zi) <= 4) {
 
-for(int linha = 0; linha < altura; linha++){
-    for(int coluna = 0; coluna < largura; coluna++){
+                double zr_novo = zr*zr - zi*zi + cr;
+                double zi_novo = 2*zr*zi + ci;
+                zr = zr_novo;
+                zi = zi_novo;
+                num_interacoes++;
 
-        //calculo do c desse pixel
-        double cr = -2.0 + (coluna / (double)(largura - 1)) * 3.0;
-        double ci = -1.5 + (linha / (double)(altura - 1)) * 3.0;
-
-        //zera as variaves
-        double zr = 0, zi = 0;
-        int num_interacoes = 0;
-        //esse (zr*zr + zi*zi) <= 4 e uma forma simplificada de ver se o numero saiu do conjunto de mandlebrot
-        //sem ter que depender de raiz quadrada
-        while (num_interacoes < max_num_interacoes && (zr*zr + zi*zi) <= 4) {
-
-            double zr_novo = zr*zr - zi*zi + cr;
-            double zi_novo = 2*zr*zi + ci;
-            zr = zr_novo;
-            zi = zi_novo;
-            num_interacoes++;
+            }
 
         }
-
     }
+
 }
 
+int main(){
     return 0;
 }
