f3ccaf8a9a7db442dd537848a0346c793aeeadc8
Juan Riquelme Xavier de Souza <juanriquelmexs2007@gmail.com>
2026-08-28 10:56:49 -0300

    adicao da funcoa openmp e atualizacao da main

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <omp.h>
 
 //a linha abaixo e o necessario para virar opemmp, mas vou ver isso melhor amanha
 //#pragma omp parallel for num_threads(num_threads)
@@ -32,7 +32,34 @@ void mandelbrot_serial(unsigned char *pixels, int largura, int altura, int max_n
     }
 
 }
-mandelbrot_openmp(unsigned char *pixels, int largura, int altura, int max_num_interacoes){
+
+void mandelbrot_openmp(unsigned char *pixels, int largura, int altura, int max_num_interacoes, int num_threads){
+    #pragma omp parallel for num_threads(num_threads)
+    for(int linha = 0; linha < altura; linha++){
+        for(int coluna = 0; coluna < largura; coluna++){
+
+            //calculo do c desse pixel
+            double cr = -2.0 + (coluna / (double)(largura - 1)) * 3.0;
+            double ci = -1.5 + (linha / (double)(altura - 1)) * 3.0;
+
+            //zera as variaves
+            double zr = 0, zi = 0;
+            int num_interacoes = 0;
+            //esse (zr*zr + zi*zi) <= 4 e uma forma simplificada de ver se o numero saiu do conjunto de mandlebrot
+            //sem ter que depender de raiz quadrada
+            while (num_interacoes < max_num_interacoes && (zr*zr + zi*zi) <= 4) {
+
+                double zr_novo = zr*zr - zi*zi + cr;
+                double zi_novo = 2*zr*zi + ci;
+                zr = zr_novo;
+                zi = zi_novo;
+                num_interacoes++;
+
+            }
+            unsigned char intensidade = (unsigned char)((num_interacoes / (double)max_num_interacoes) * 255);
+            pixels[linha * largura + coluna] = intensidade;
+        }
+    }
     
 }
 
@@ -71,6 +98,7 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Erro: parâmetros devem ser inteiros positivos\n");
         return 1;
     }
+
     //unsigned char nao tem sinal, sou seja vai de 0 a 255, justamente os valores RGB
     unsigned char *pixels = malloc(largura * altura * sizeof(unsigned char));
     if (pixels == NULL) {
@@ -78,9 +106,18 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    // --- Serial ---
     mandelbrot_serial(pixels, largura, altura, max_num_interacoes);
 
-    if (!save_pgm("mandelbrot_login_serial.pgm", pixels, largura, altura)) {
+    if (!save_pgm("mandelbrot_jrxs_serial.pgm", pixels, largura, altura)) {
+        free(pixels);
+        return 1;
+    }
+
+    // --- OpenMP ---
+    mandelbrot_openmp(pixels, largura, altura, max_num_interacoes, num_threads);
+
+    if (!save_pgm("mandelbrot_jrxs_openmp.pgm", pixels, largura, altura)) {
         free(pixels);
         return 1;
     }
